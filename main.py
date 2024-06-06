30873bf9208c34809833fdf1fed2d1756e462024
Blake Nall <blake@proton.me>
2024-06-06 13:14:51 -0500

    Added encodedurl support

--- main.py
+++ main.py
@@ -1,7 +1,13 @@
+import logging
+from json.decoder import JSONDecodeError
+
 from fastapi import FastAPI, Request
+from fastapi.responses import JSONResponse
 
 from integrations.twilio import TwilioCaller
 from models import NotificationRequest, NotificationResponse
+
+logger = logging.getLogger(__name__)
 
 
 def clean_to_number(to_number: str) -> list[str] | str:
@@ -19,24 +25,24 @@ class TerminusNotifierApp:
 
         return None
 
-    async def log_post_data(self, request: Request):
-        if request.method == "POST":
-            body = await request.json()
-            print(f"POST data: {body}")
-
-        return request
-
     def create_routes(self) -> None:
         @self.app.post("/notify/{method}")
         async def notify(
-            request: NotificationRequest,
+            request: Request,
             method: str = "call",
         ) -> NotificationResponse:
             """Send a notification to phone numbers using Twilio."""
-            caller = TwilioCaller()
-            to_number = clean_to_number(request.to_number)
-            message = request.message
+            content_type = request.headers.get("content-type")
+            if content_type == "application/x-www-form-urlencoded":
+                form = await request.form()
+                data = dict(form)
+            else:
+                data = await request.json()
 
+            to_number = clean_to_number(data.get("to_number"))
+            message = data.get("message")
+
+            caller = TwilioCaller()
             if method in caller.valid_methods:
                 if isinstance(to_number, list):
                     await caller.batch_message(

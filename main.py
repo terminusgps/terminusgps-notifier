d3b2b82f21763aae56e49fe7763b8b668ad4b30d
Blake Nall <blake@proton.me>
2024-10-03 12:06:30 -0500

    Added url-encoded data processing

--- main.py
+++ main.py
@@ -1,13 +1,12 @@
 import asyncio
-from typing import Any
+from typing import Annotated, Any, Optional
 from asyncio.tasks import Task
-from fastapi import FastAPI
+from fastapi import FastAPI, Form
 from twilio.base.exceptions import TwilioRestException
 from wialon.api import WialonError
 
 from caller import TwilioCaller
 from models import NotificationResponse, NotificationErrorResponse
-from models.requests import NotificationRequest
 from wialonapi import WialonSession, WialonUnit
 from wialonapi.items.unit import clean_phone_numbers
 
@@ -30,33 +29,35 @@ def create_tasks(
 @app.post("/notify/{method}")
 async def notify(
     method: str,
-    notification: NotificationRequest,
+    message: Annotated[str, Form()],
+    to_number: Annotated[Optional[str], Form()] = None,
+    unit_id: Annotated[Optional[int], Form()] = None,
 ) -> NotificationResponse | NotificationErrorResponse:
     """Send a notification to phone numbers using Twilio."""
     phone_numbers = []
-    if not notification.to_number and not notification.unit_id:
+    if not to_number and not unit_id:
         return NotificationErrorResponse(
             phones=phone_numbers,
-            unit_id=str(notification.unit_id),
-            message=notification.message,
+            unit_id=str(unit_id),
+            message=message,
             method=method,
             error="",
             error_desc="No unit_id or to_number provided.",
         )
 
-    if notification.to_number:
-        phone_numbers.extend(clean_phone_numbers([notification.to_number]))
-    if notification.unit_id:
+    if to_number:
+        phone_numbers.extend(clean_phone_numbers([to_number]))
+    if unit_id:
         try:
             with WialonSession() as session:
-                unit = WialonUnit(id=str(notification.unit_id), session=session)
+                unit = WialonUnit(id=str(unit_id), session=session)
                 unit_phones = clean_phone_numbers(unit.get_phone_numbers())
                 phone_numbers.extend(unit_phones)
         except WialonError as e:
             return NotificationErrorResponse(
                 phones=phone_numbers,
-                unit_id=str(notification.unit_id),
-                message=notification.message,
+                unit_id=str(unit_id),
+                message=message,
                 method=method,
                 error=str(e),
                 error_desc="Something went wrong with Wialon.",
@@ -66,7 +67,7 @@ async def notify(
         with TwilioCaller() as caller:
             tasks: list[Task[Any]] = create_tasks(
                 phone_numbers=phone_numbers,
-                message=notification.message,
+                message=message,
                 method=method,
                 caller=caller,
             )
@@ -74,8 +75,8 @@ async def notify(
     except TwilioRestException as e:
         return NotificationErrorResponse(
             phones=phone_numbers,
-            unit_id=str(notification.unit_id),
-            message=notification.message,
+            unit_id=str(unit_id),
+            message=message,
             method=method,
             error=str(e),
             error_desc="Something went wrong with Twilio.",
@@ -83,7 +84,7 @@ async def notify(
     else:
         return NotificationResponse(
             phones=phone_numbers,
-            unit_id=str(notification.unit_id),
-            message=notification.message,
+            unit_id=str(unit_id),
+            message=message,
             method=method,
         )
--- models/requests.py
+++ models/requests.py
@@ -1,7 +1,9 @@
+from fastapi import Form
+from typing import Annotated, Optional
 from pydantic import BaseModel
 
 
 class NotificationRequest(BaseModel):
-    unit_id: int | None = None
-    to_number: str | None = None
-    message: str
+    unit_id: Annotated[Optional[int], Form()] = Form(...)
+    to_number: Annotated[Optional[str], Form()] = Form(...)
+    message: Annotated[str, Form()]

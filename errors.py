0e07c41dfcaf5d2d6c787e082a505eecc93b7346
darthnall <blake@proton.me>
2025-07-21 10:06:36 -0500

    Remove fastapi files

--- errors.py
+++
@@ -1,6 +0,0 @@
-class InvalidTwilioMethodError(Exception):
-    """Raised when an invalid Twilio method is supplied to a TwilioCaller instance."""
-
-
-class InvalidPhoneNumber(Exception):
-    """Raised when a phone number is invalid for use with the Twilio API."""

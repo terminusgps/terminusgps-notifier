961b945418cc7b44da94c0619af507fa04e8bf72
darthnall <blake@proton.me>
2025-08-11 13:26:58 -0500

    Reenabled middleware

--- src/settings/prod.py
+++ src/settings/prod.py
@@ -67,6 +67,8 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    "django.middleware.security.SecurityMiddleware",
     "django.middleware.common.CommonMiddleware",
+    "django.middleware.csrf.CsrfViewMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
 ]

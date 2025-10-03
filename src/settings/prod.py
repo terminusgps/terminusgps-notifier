cc0ec0f2fc9d6035e2293239c4d2a16d98289ec8
darthnall <blake@proton.me>
2025-10-03 14:59:57 -0500

    Updated middleware

--- src/settings/prod.py
+++ src/settings/prod.py
@@ -112,6 +112,7 @@ TEMPLATES = [
 
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
+    "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
     "django.middleware.csrf.CsrfViewMiddleware",
     "django.contrib.auth.middleware.AuthenticationMiddleware",

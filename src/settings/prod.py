9f51eec4dab9f55e6b83f31315da9d2bfe9b3985
darthnall <blake@proton.me>
2025-10-03 14:54:04 -0500

    Removed psycopg

--- src/settings/prod.py
+++ src/settings/prod.py
@@ -68,16 +68,22 @@ LOGGING = {
 }
 
 
+# DATABASES = {
+#     "default": {
+#         "ENGINE": "django.db.backends.postgresql",
+#         "NAME": os.getenv("DB_NAME"),
+#         "HOST": os.getenv("DB_HOST"),
+#         "USER": os.getenv("DB_USER"),
+#         "PASSWORD": os.getenv("DB_PASSWORD"),
+#         "PORT": os.getenv("DB_PORT", 5432),
+#         "OPTIONS": {"client_encoding": "UTF8"},
+#         "CONN_MAX_AGE": None,
+#     }
+# }
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.postgresql",
-        "NAME": os.getenv("DB_NAME"),
-        "HOST": os.getenv("DB_HOST"),
-        "USER": os.getenv("DB_USER"),
-        "PASSWORD": os.getenv("DB_PASSWORD"),
-        "PORT": os.getenv("DB_PORT", 5432),
-        "OPTIONS": {"client_encoding": "UTF8"},
-        "CONN_MAX_AGE": None,
+        "ENGINE": "django.db.backends.sqlite3",
+        "NAME": BASE_DIR / "db.sqlite3",
     }
 }
 

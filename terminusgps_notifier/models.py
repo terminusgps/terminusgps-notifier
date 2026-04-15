cca9a0f6555e37597cbfb0de751070e70650278f
Blake Nall <blake@terminusgps.com>
2026-04-15 12:56:48 -0500

    Removed useless property from customer model

--- terminusgps_notifier/models.py
+++ terminusgps_notifier/models.py
@@ -1,5 +1,3 @@
-from functools import cached_property
-
 from django.contrib.auth import get_user_model
 from django.db import models
 from django.utils.translation import gettext_lazy as _
@@ -7,14 +5,14 @@ from encrypted_field import EncryptedField
 
 
 class TerminusGPSNotifierCustomer(models.Model):
-    messages_count = models.PositiveIntegerField(default=0)
-    messages_limit = models.PositiveIntegerField(default=500)
-    token = EncryptedField(blank=True, null=True, default=None)
-    user = models.ForeignKey(
+    user = models.OneToOneField(
         get_user_model(),
         on_delete=models.CASCADE,
         related_name="notifier_customer",
     )
+    token = EncryptedField(blank=True, null=True, default=None)
+    messages_count = models.PositiveIntegerField(default=0)
+    messages_limit = models.PositiveIntegerField(default=500)
     subscription = models.ForeignKey(
         "terminusgps_payments.Subscription",
         blank=True,
@@ -30,7 +28,3 @@ class TerminusGPSNotifierCustomer(models.Model):
 
     def __str__(self) -> str:
         return str(self.user)
-
-    @cached_property
-    def has_token(self) -> bool:
-        return self.token is not None

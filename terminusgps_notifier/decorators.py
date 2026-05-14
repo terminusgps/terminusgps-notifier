acf52e178e57c18aaf103527508a35514c6bcc02
Blake Nall <blake@terminusgps.com>
2026-05-14 12:52:03 -0500

    Updated subscription decorator

--- terminusgps_notifier/decorators.py
+++ terminusgps_notifier/decorators.py
@@ -6,19 +6,17 @@ from django.contrib import messages
 from django.contrib.auth.models import AbstractBaseUser
 from django.http import HttpRequest, HttpResponse
 from django.shortcuts import get_object_or_404, redirect
-from terminusgps.wialon.session import WialonSession
-from wialon.api import WialonError
 
 from .models import Profile
 
-__all__ = ["htmx_template", "wialon_session"]
+__all__ = ["htmx_template", "active_subscription_required"]
 
 
 class HtmxHttpRequest(HttpRequest):
     template_name: str
 
 
-def valid_subscription_required():
+def active_subscription_required(view_func=None):
     def user_has_subscription(user: AbstractBaseUser) -> bool:
         if user.is_anonymous:
             return False
@@ -26,90 +24,31 @@ def valid_subscription_required():
             profile = get_object_or_404(Profile, user=user)
             return profile.subscription_id is not None
 
-    def get_subscription_status(user: AbstractBaseUser) -> str:
+    def get_subscription_from_stripe(user: AbstractBaseUser) -> dict:
         profile = get_object_or_404(Profile, user=user)
-        client = stripe.StripeClient(settings.STRIPE_API_KEY)
-        subscription = client.v1.subscriptions.retrieve(
-            profile.subscription_id
-        )
-        return str(subscription.status)
+        subscription_id = profile.subscription_id
+        stripe_client = stripe.StripeClient(settings.STRIPE_API_KEY)
+        subscription = stripe_client.v1.subscriptions.retrieve(subscription_id)
+        return subscription.to_dict()
 
     def outer_wrapper(view_func):
         @functools.wraps(view_func)
         def inner_wrapper(request, *args, **kwargs) -> HttpResponse:
             if user := getattr(request, "user", None):
                 if user_has_subscription(user):
-                    status = get_subscription_status(user)
-                    print(f"{status = }")
-                    return view_func(request, *args, **kwargs)
+                    subscription = get_subscription_from_stripe(user)
+                    if subscription.get("status", "expired") == "active":
+                        return view_func(request, *args, **kwargs)
             msg = "You need to subscribe to do that."
             messages.warning(request, msg)
             return redirect("terminusgps_notifier:dashboard")
 
         return inner_wrapper
 
-    return outer_wrapper
-
-
-def wialon_session():
-    def get_wialon_api_token(request: HttpRequest) -> str | None:
-        if not hasattr(request, "user"):
-            return
-        if request.user.is_anonymous:
-            return
-        profile = get_object_or_404(Profile, user=request.user)
-        return profile.token
-
-    def wialon_sid_is_valid(sid: str | None) -> bool:
-        session = WialonSession(sid=sid)
-        try:
-            session.wialon_api.avl_evts()
-            return True
-        except WialonError:
-            return False
-
-    def outer_wrapper(view_func):
-        @functools.wraps(view_func)
-        def inner_wrapper(request, *args, **kwargs):
-            wialon_api_token = get_wialon_api_token(request)
-            if not wialon_api_token:
-                messages.warning(
-                    request,
-                    "You need to connect your Wialon account to do that.",
-                )
-                return redirect("terminusgps_notifier:dashboard")
-            if not wialon_sid_is_valid(request.session.get("wialon_sid")):
-                session = WialonSession(sid=None)
-                session.token_login(token=wialon_api_token)
-                request.session["wialon_sid"] = session.id
-            return view_func(request, *args, **kwargs)
-
-        return inner_wrapper
-
-    return outer_wrapper
-
-
-def wialon_token_required():
-    def user_has_wialon_token(user: AbstractBaseUser) -> bool:
-        if user.is_anonymous:
-            return False
-        else:
-            profile = get_object_or_404(Profile, user=user)
-            return bool(profile.token)
-
-    def outer_wrapper(view_func):
-        @functools.wraps(view_func)
-        def inner_wrapper(request, *args, **kwargs) -> HttpResponse:
-            if user := getattr(request, "user", None):
-                if user_has_wialon_token(user):
-                    return view_func(request, *args, **kwargs)
-            msg = "You need to connect your Wialon account to do that."
-            messages.warning(request, msg)
-            return redirect("terminusgps_notifier:dashboard")
-
-        return inner_wrapper
-
-    return outer_wrapper
+    if view_func is None:
+        return outer_wrapper
+    else:
+        return outer_wrapper(view_func)
 
 
 def htmx_template(template_name: str):

5cc434cc525945eb9373ba658440984440c4da74
Blake Nall <blake@terminusgps.com>
2026-06-01 10:50:18 -0500

    Removed separate validators file

--- terminusgps_notifier/wialon.py
+++ terminusgps_notifier/wialon.py
@@ -4,12 +4,43 @@ from functools import partial
 
 from django.core.cache import cache
 from django.core.exceptions import ValidationError
+from django.utils.translation import gettext_lazy as _
 from terminusgps.wialon import flags
 from terminusgps.wialon.session import WialonAPIError, WialonSession
 
-from terminusgps_notifier.validators import validate_e164_phone_number
-
 logger = logging.getLogger(__name__)
+
+
+def validate_e164_phone_number(value: str) -> None:
+    """Raises :py:exec:`~django.core.exceptions.ValidationError` if the value wasn't a properly formatted E.164 phone number."""
+    if not value:
+        raise ValidationError(
+            "Invalid E.164 phone number: '%(value)s'",
+            code="invalid",
+            params={"value": value},
+        )
+    if not value.startswith("+"):
+        raise ValidationError(
+            _("E.164 phone number must start with a '+', got '%(char)s'"),
+            code="invalid",
+            params={"char": value[0]},
+        )
+    if not value.removeprefix("+").isdigit():
+        raise ValidationError(
+            _(
+                "E.164 phone number must be entirely digits following '+', got '%(value)s'"
+            ),
+            code="invalid",
+            params={"value": value.removeprefix("+")},
+        )
+    if len(value.removeprefix("+")) > 15:
+        raise ValidationError(
+            _(
+                "E.164 phone number cannot be greater than 15 characters in length, got %(len)s."
+            ),
+            code="invalid",
+            params={"len": len(value.removeprefix("+"))},
+        )
 
 
 def get_session(sid: str) -> WialonSession:

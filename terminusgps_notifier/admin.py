9a68a93709b837b9da54908f2c680aba3db71b45
Blake Nall <blake@terminusgps.com>
2026-06-08 11:14:28 -0500

    Updated admin

--- terminusgps_notifier/admin.py
+++ terminusgps_notifier/admin.py
@@ -6,3 +6,8 @@ from . import models
 @admin.register(models.Profile)
 class ProfileAdmin(admin.ModelAdmin):
     list_display = ["user", "messages_count", "messages_limit"]
+
+
+@admin.register(models.DispatchLog)
+class DispatchLogAdmin(admin.ModelAdmin):
+    list_display = ["unit_id", "method", "message"]
